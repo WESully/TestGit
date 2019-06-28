0605cdf589467dbeb85c14f6cc7fe18cee91a805
William Sullivan <uwash@washcard.com>
2019-06-28 11:23:26 -0500

    Initial Commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by William Sullivan on 6/28/19.
+//  Copyright © 2019 WashCard Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
